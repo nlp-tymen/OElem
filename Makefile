ce193932da9aadcccb90452d4d0879374804389b
Alexander Efimov <alexandr.efimov@gmail.com>
2020-02-29 14:12:55 +0500

    rename repo

---
+++ Makefile
@@ -0,0 +1,8 @@
+main serve:
+	yarn serve
+
+deploy:
+	./deploy.sh
+
+install:
+	yarn install
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
-        ? '/nlp_vue/'
+        ? '/OElem/'
         : '/'
    }
