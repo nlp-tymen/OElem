b387b7d3b7c69fc690c468231e1f19aee25fbdac
Elena Efimova <elena.efimova@gmail.com>
2020-03-01 14:13:16 +0500

    .

--- src/App.vue
+++ src/App.vue
@@ -15,14 +15,15 @@
             </b-nav-form>
         
           </b-navbar-nav>
-        </b-collapse>
       </b-navbar>
 
-<b-container class="bv-example-row">
+<b-container class="bv-example-row" >
   
   <div class="container">
     <div class="row">
-        <div class=" col-5 mx-auto my-2 my-sm-3 my-lg-4 p-3 " style="height: 100px;">Old English Lemmatizer</div>
+        <div class=" col-10 mx-auto my-2 my-sm-3 my-lg-4 p-3 " style="height: 100px;">
+          <h1> Old English Lemmatizer </h1>
+          </div>
         
     </div>
 </div>
@@ -31,16 +32,27 @@
 
 <div>
   <b-navbar type="light" variant="light" class=" mx-auto my-2 my-sm-3 my-lg-4 p-3 ">
+    <b-container class="bv-example-row mb-3" fluid>
+      <b-row cols="2" >
+    <b-col>
     <b-nav-form>
       <b-form-input class="mr-sm-2" placeholder="Add your corpus here"></b-form-input>
       <b-button variant="outline-success" class="my-2 my-sm-0" type="submit">Upload</b-button>
     </b-nav-form>
+    </b-col>
+
+    <b-col >
     <b-nav-form>
       <b-form-input class="pl-sm-10" placeholder="Enter your word"></b-form-input>
       <b-button variant="outline-success" class="my-2 my-sm-0" type="submit">Search</b-button>
     </b-nav-form>
+        </b-col>
+        
+</b-row>
+</b-container>
   </b-navbar>
 </div>
+
 
 <div class="container">
  <div class=" col-10 mx-auto my-2 my-sm-3 my-lg-4 p-3 " style="height: 100px;"> 
@@ -49,13 +61,46 @@
     <b-tab title="Lemmatization" active>
       <p>
         <template>
-  <div>
-    <b-table striped hover :items="items"></b-table>
-  </div>
+                  <div class="container">
+                            
+                  <table class="table table-hover">
+                    <thead>
+                      <tr>
+                        <th>Frequency</th>
+                        <th>Word</th>
+                        <th>Lemma</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td>196</td>
+                        <td>ādilgast</td>
+                        <td>ādilgian</td>
+                      </tr>
+                      <tr>
+                        <td>78</td>
+                        <td>wæl</td>
+                        <td>wæl</td>
+                      </tr>
+                      <tr>
+                        <td> </td>
+                        <td> </td>
+                        <td> </td>
+                      </tr>
+                      <tr>
+                        <td> </td>
+                        <td> </td>
+                        <td> </td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+
 </template>
 
 
-        </p></b-tab>
+        </p>
+        </b-tab>
     <b-tab title="Stemming"><p>I'm the second tab</p></b-tab>
     <b-tab title="POS tagging"><p>I'm a disabled tab!</p></b-tab>    
     <b-tab title="Word list"><p>I'm a disabled tab!</p></b-tab>
@@ -66,62 +111,6 @@
 </div>
 
 
-  <b-row>
-    <b-col>
-
-      <b-card no-body header="Card with flush list group">
-    <b-list-group flush>
-      <b-list-group-item href="#">Cras justo odio</b-list-group-item>
-      <b-list-group-item href="#">Dapibus ac facilisis in</b-list-group-item>
-      <b-list-group-item href="#">Vestibulum at eros</b-list-group-item>
-    </b-list-group>
-
-    <b-card-body>
-      Quis magna Lorem anim amet ipsum do mollit sit cillum voluptate ex nulla tempor. Laborum
-      consequat non elit enim exercitation cillum aliqua consequat id aliqua. Esse ex consectetur
-      mollit voluptate est in duis laboris ad sit ipsum anim Lorem.
-    </b-card-body>
-  </b-card>
-</b-card-group>
-
-    </b-col>
-    <b-col>
-     <b-card no-body header="Card with flush list group">
-    <b-list-group flush>
-      <b-list-group-item href="#">Cras justo odio</b-list-group-item>
-      <b-list-group-item href="#">Dapibus ac facilisis in</b-list-group-item>
-      <b-list-group-item href="#">Vestibulum at eros</b-list-group-item>
-    </b-list-group>
-
-    <b-card-body>
-      Quis magna Lorem anim amet ipsum do mollit sit cillum voluptate ex nulla tempor. Laborum
-      consequat non elit enim exercitation cillum aliqua consequat id aliqua. Esse ex consectetur
-      mollit voluptate est in duis laboris ad sit ipsum anim Lorem.
-    </b-card-body>
-  </b-card>
-</b-card-group> 
-       </b-col>
-
-
-    <b-col>
-
-<b-card no-body header="Card with flush list group">
-    <b-list-group flush>
-      <b-list-group-item href="#">Cras justo odio</b-list-group-item>
-      <b-list-group-item href="#">Dapibus ac facilisis in</b-list-group-item>
-      <b-list-group-item href="#">Vestibulum at eros</b-list-group-item>
-    </b-list-group>
-
-    <b-card-body>
-      Quis magna Lorem anim amet ipsum do mollit sit cillum voluptate ex nulla tempor. Laborum
-      consequat non elit enim exercitation cillum aliqua consequat id aliqua. Esse ex consectetur
-      mollit voluptate est in duis laboris ad sit ipsum anim Lorem.
-    </b-card-body>
-  </b-card>
-</b-card-group>
-
-    </b-col>
-  </b-row>
 </b-container>
 
     
